b914a97f25967cf1ee3ea1b7ca68a814b0f28b52
phkhang <trannguyenphuckhang12@gmail.com>
2025-01-07 08:35:35 +0700

    Init repository

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        from: "Express server",
+        status: "✅Still online",
+        message: 'Hello World. Programmed to work but not to feel',
+        "time-stamp": new Date().toISOString()
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.send(`
+        <h1>About page</h1>
+        <p>This is a simple about page</p>
+        <br>
+        <h3>Made purely for learning purposes</h3>
+        `);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
