3bf9ec38ba3d3fcb5a4b78e81b9b6edf9376fb23
thiennbao <thiennbao.204@gmail.com>
2025-01-10 22:45:31 +0700

    Add: hehe page

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import expressHbs from 'express-handlebars';
-import 'dotenv/config'
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import expressHbs from "express-handlebars";
+import "dotenv/config";
 
 const port = process.env.PORT || 3000;
 
@@ -12,59 +12,64 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Set up template engine
-app.engine('hbs', expressHbs.engine(
-    {
-        layoutsDir: 'views/layouts',
-        partialsDir: 'views/partials',
-        extname: 'hbs',
-        defaultLayout: 'main-layout',
-        runtimeOptions: {
-            allowProtoPropertiesByDefault: true,
-            // allowProtoMethodsByDefault: true
-        }
-    }
-));
-app.set('view engine', 'hbs');
+app.engine(
+  "hbs",
+  expressHbs.engine({
+    layoutsDir: "views/layouts",
+    partialsDir: "views/partials",
+    extname: "hbs",
+    defaultLayout: "main-layout",
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      // allowProtoMethodsByDefault: true
+    },
+  })
+);
+app.set("view engine", "hbs");
 
-app.get('/', (req, res) => {
-    res.render('index', {});
+app.get("/", (req, res) => {
+  res.render("index", {});
 });
 
-app.get('/test', (req, res) => {
-    res.json({
-        from: "ExpressJS server",
-        "to-be": "or not to be",
-        status: "✅Still online",
-        message: 'Hello World. Programmed to work but not to feel',
-        "time-stamp": new Date().toISOString()
-    });
+app.get("/test", (req, res) => {
+  res.json({
+    from: "ExpressJS server",
+    "to-be": "or not to be",
+    status: "✅Still online",
+    message: "Hello World. Programmed to work but not to feel",
+    "time-stamp": new Date().toISOString(),
+  });
 });
 
 app.get("/about", (req, res) => {
-    res.send(`
+  res.send(`
         <h1>About page</h1>
         <p>This is a simple about page</p>
         <br>
         <h3>Made purely for learning purposes</h3>
         <p>For collaboration and more</p>
         `);
-})
+});
 
 app.get("/what", (req, res) => {
-    res.send(`
+  res.send(`
         <h1>Yes</h1>
         `);
-})
+});
 
 app.get("/meo", (req, res) => {
-    res.send(`
+  res.send(`
         <h1>Meo page</h1>
         
         <p>This is a simple meo page</p>
         <img src='https://avatars.githubusercontent.com/u/84757707?v=4'/>
-        `)
-})
+        `);
+});
+
+app.get("/hehe", (req, res) => {
+  res.json({ message: "he".repeat(1000000) });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
