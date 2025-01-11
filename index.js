2d2628d475367772ae2c92207827401c4cd80d93
ntploc21 <ntploc21@gmail.com>
2025-01-11 21:05:53 +0700

    Update: update /test endpoint.

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ app.get("/", (req, res) => {
 
 app.get("/test", (req, res) => {
   res.json({
+    from: "Express server with ES6",
     status: "✅ Online",
+    message: "Hello World. GDSC is a club for developers",
     "time-stamp": new Date().toISOString(),
   });
 });
